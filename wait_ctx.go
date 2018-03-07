92e9f7728bec888f3d97210368f64ce38242f853
Rakesh Nair <raknair07@gmail.com>
2018-03-06 17:45:55 -0800

    implement wait ctx

--- wait_ctx.go
+++ wait_ctx.go
@@ -1,1 +1,46 @@
-package wait_ctx
+package waitctx
+
+import (
+	"context"
+	"log"
+	"sync"
+)
+
+var once sync.Once
+var wc *WaitCtx
+
+func ServiceCtx(ctx context.Context) *WaitCtx {
+	once.Do(func() {
+		log.Println("waitCtx registered")
+		var wg sync.WaitGroup
+		wc = &WaitCtx{
+			ctx: ctx,
+			wg:  &wg,
+		}
+	})
+	return wc
+}
+
+func RoutineCtx(ctx context.Context) *WaitCtx {
+	wc := ServiceCtx(ctx)
+	log.Println("new routine added to service waitCtx")
+	wc.wg.Add(1)
+	return wc
+}
+
+type WaitCtx struct {
+	ctx context.Context
+	wg  *sync.WaitGroup
+}
+
+func (wc *WaitCtx) Stop() <-chan struct{} {
+	return wc.ctx.Done()
+}
+
+func (wc *WaitCtx) MarkAsDone() {
+	wc.wg.Done()
+}
+
+func (wc *WaitCtx) Wait() {
+	wc.wg.Wait()
+}
